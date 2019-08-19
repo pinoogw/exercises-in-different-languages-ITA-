400588a5696c04bf90a0eab34b532f2fdf00f4a9
Wrath-After <53044683+Wrath-After@users.noreply.github.com>
2019-08-19 16:47:17 +0200

    Add files via upload

---
+++ python/informazioni.py
@@ -0,0 +1,15 @@
+import socket
+import requests
+import uuid
+import platform
+ip = requests.get("https://ip.42.pl/anything%20.")
+print ("il tuo ip pubblico ==> " + ip.text)
+hostname = socket.gethostname()    
+IPAddr = socket.gethostbyname(hostname)    
+print("il nome del tuo pc ===> " + hostname)    
+print("l'indirizzo ip del tuo computer ===> " + IPAddr) 
+print ("l'indirizzo mac è : ", end="") 
+print (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) 
+for ele in range(0,8*6,8)][::-1]))
+print("il tuo sistema operativo è ===> " + platform.platform())
+print("il tuo processore è ===> " + platform.processor())
